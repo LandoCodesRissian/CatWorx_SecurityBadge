425567ecefeb1f342cba17ce1f171d2850e1728c
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2024-02-18 23:19:53 -0700

    util class set up with printEmployee method

--- Catworx.BadgeMaker/Program.cs
+++ Catworx.BadgeMaker/Program.cs
@@ -1,8 +1,18 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
+    class Util
+    {
+        public static void PrintEmployees(List<Employee> employees) {
+            for (int i=0; i < employees.Count; i++)
+            {
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+            }
+        }
+    }
     class Program
     {
 static List<Employee> GetEmployees()
@@ -36,21 +46,10 @@ static List<Employee> GetEmployees()
 
     return employees;
 }
-
-
-        static void PrintEmployees(List<Employee> employees)
-        {
-            for (int i = 0; i < employees.Count; i++)
-            {
-                string template = "{0,-10}\t{1,-20}\t{2}";
-                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName, employees[i].GetPhotoUrl()));
-            }
-        }
-
         static void Main(string[] args)
         {
             List<Employee> employees = GetEmployees();
-            PrintEmployees(employees);
+            Util.PrintEmployees(employees);
         }
     }
 }
--- Catworx.BadgeMaker/obj/Debug/net8.0/Catworx.BadgeMaker.AssemblyInfo.cs
+++ Catworx.BadgeMaker/obj/Debug/net8.0/Catworx.BadgeMaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catworx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f924d68e6b984ffdfd8a11b579ea6adae4ea27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92d63c1a277fbea1ce8bfab68df656b12fcbc672")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catworx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catworx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
